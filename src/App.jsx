f7fc44057cbf713e165d5c916a49dc719ad92d4a
almeald <almenaldana@gmail.com>
2020-09-01 17:34:15 -0500

    estructurando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import {firebase} from './firebase';
+import React, { useState, Children } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import MainLayout from './components/layout/MainLayout'
+import SimpleLayout from './components/layout/SimpleLayout'
+import LoginContainer from './containers/LoginContainer'
+import HomeContainer from './containers/HomeContainer'
+import CommunityContainer from './containers/CommunityContainer'
+import TrainingsContainer from './containers/TrainingsContainer'
+import ChatContainer from './containers/ChatContainer'
+import ProfileContainer from './containers/ProfileContainer'
+
 
 
 function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true)
 
-  const [emprendimiento, setEmprendimiento] = useState([]);
+  function LoggedInRoute({ children, ...rest }) {
 
-  useEffect(() => {
-    const getData = async() => {
-      try {
-        const db = firebase.firestore();
-        const data = await db.collection('emprendimientos').get();
-        console.log(data.docs);
-        // const arrayData = data.map(() => {
-          
-        // })
+    return (
+      <Route
+        {...rest}
+        render={() => {
+          if (isUserLoggedIn) {
+            return <MainLayout>{children}</MainLayout>
+          }
+          return <Redirect to='/login' />
+        }
+        }
+      >
+      </Route>
+    );
+  }
 
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getData();
-  }, []);
 
   return (
-    <div>
-      <h2>banana</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/login' component={LoginContainer} />
+        <LoggedInRoute path='/home'>
+          <HomeContainer />
+        </LoggedInRoute>
+        <LoggedInRoute path='/community'>
+          <CommunityContainer />
+        </LoggedInRoute>
+        <LoggedInRoute path='/trainings'>
+          <TrainingsContainer />
+        </LoggedInRoute>
+        <LoggedInRoute path='/chat'>
+          <ChatContainer />
+        </LoggedInRoute>
+        <LoggedInRoute path='/profile'>
+          <ProfileContainer />
+        </LoggedInRoute>
+      </Switch>
+    </Router>
   );
 }
 
