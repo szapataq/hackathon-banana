8f9ec1f21aa24b9d126a3efff54678bc98c9de9c
almeald <almenaldana@gmail.com>
2020-09-01 17:30:45 -0500

    creando el contenido de los componentes

---
+++ src/containers/LoginContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SimpleLayout from '../components/layout/SimpleLayout'
+
+
+function LoginContainer() {
+  return (
+    <SimpleLayout>
+      <div>
+        HomeContainer
+        //aqio ira el contenido de login
+      </div>
+    </SimpleLayout>
+  )
+}
+
+export default LoginContainer
---
+++ src/containers/TrainingsContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/layout/MainLayout'
+
+function TrainingsContainer() {
+  return (
+    <MainLayout>
+      TrainingsContainer
+    </MainLayout>
+  )
+}
+
+export default TrainingsContainer
