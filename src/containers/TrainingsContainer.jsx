80dc9ec2c42f35a72f875f7e9c21049ca517ba1e
szapataq <szapata013@gmail.com>
2020-09-02 09:52:42 -0500

    cambios css

--- src/containers/TrainingsContainer.jsx
+++ src/containers/TrainingsContainer.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import './trainingsContainer.scss';
+import chatIcon from '../assets/training.svg';
 
 function TrainingsContainer() {
   return (
-    <div>
-      TrainingsContainer
-    </div>
+    <>
+      <Header/>
+      <div class="container-training">
+        <div className="card-container">
+          <img src={chatIcon} alt=""/>
+          <div>
+            <p>Aprende a usar chat emprendedor</p>
+            <spam>Viernes 11/09</spam>
+          </div>
+          <img src="" alt=""/>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default TrainingsContainer
+export default TrainingsContainer;
