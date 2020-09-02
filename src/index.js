48ab1a07b825316726fb2a968605e9924ee5ae26
almeald <almenaldana@gmail.com>
2020-09-02 01:29:06 -0500

    modificando ruta de bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/scss/bootstrap.scss';
 
 ReactDOM.render(
   <React.StrictMode>
