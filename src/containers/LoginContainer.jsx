40a107b64df5b91440f6cc4b05bac44538922451
almeald <almenaldana@gmail.com>
2020-09-03 12:32:41 -0500

    agregando componenete button

--- src/containers/LoginContainer.jsx
+++ src/containers/LoginContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FormLogin from '../components/FormLogin'
+import Button from '../components/Button'
 import logo from '../assets/icons/logo.svg'
 
 function LoginContainer() {
@@ -13,6 +14,7 @@ function LoginContainer() {
         <span>Emprendiendo juntos</span>
       </div>
       <FormLogin></FormLogin>
+      <Button className="bt-login">Inicia Sesión</Button>
     </>
   )
 }
