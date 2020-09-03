6eac070d74135a90a42dccdb201d717796eb6a21
syaker <60866313+syaker@users.noreply.github.com>
2020-09-03 18:48:35 -0500

    Capturando valor del input del chat

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBt from './InputBt';
 import Message from './Message';
+import { auth } from '../firebase';
 
 const MessageContainer = () => {
+	const [ messageToSend, setMessageToSend] = useState("");
+	const captureText = (e) => {
+		setMessageToSend(e.target.value)
+	}
+	const sendMessage = (e) => {
+		e.preventDefault()
+		const user = auth.currentUser.uid;
+		//Aqui ejecutar la funcion que envia el mensaje
+	}
 	return ( 
 		<div className="message-container">
 			<Message />
-			<InputBt />
+			<InputBt onChange={captureText} onClick={sendMessage} value={messageToSend} className="input-chat" placeholder="Escribe un mensaje"/>
 		</div>
 	 );
 }
