e1a569b87d1f25b5024c0e00d250c8b718ea9282
syaker <60866313+syaker@users.noreply.github.com>
2020-09-04 04:10:23 -0500

    Modificacion de componentes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { getAllMessages, searchMessages } from '../API/message';
 import './Message.scss'
 import { auth } from '../firebase';
+import { useParams } from 'react-router-dom';
 
 const Mesagge = () => {
 
 	const user = auth.currentUser.uid;
-	const idReceiver = "sPQSWWtl1wdpveD83beK2RjomPF2";
+	let { id:idReceiver }= useParams();
 	const [allMessages, setAllMessages] = useState([])
 	const getMessages = async () => await getAllMessages(user, idReceiver);
+
 	useEffect(() => {
-		getMessages().then((messages) => {
-			const promises = messages.map((objChat) => {
-				if (objChat.IDReceiver === objChat.queryReceiver) {
-					return searchMessages(objChat.IDChat)
-				}
-			})
-			return Promise.all(promises)
-		}).then((messages) => setAllMessages(messages[0]))
+		getMessages().then((chatInfo) => {
+			console.log(chatInfo)
+			if (chatInfo === []) {
+				return []
+			} else {
+				const promises = chatInfo.map((objChat) => {
+					if (objChat.IDReceiver === objChat.queryReceiver) {
+						return searchMessages(objChat.IDChat)
+					}
+				})
+				return Promise.all(promises)
+			}
+		}).then((messages) => {
+			if (messages.length === 0) {
+				setAllMessages([{
+					message: "Bienvenid@ a un nuevo chat",
+					date: ""
+				}])
+			} else {
+				setAllMessages(messages[0][0])
+			}
+		})
 	}, []);
 
 	return (
