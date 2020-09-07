4dc8deb97bac8a04e4938a1a27809e61b81d3256
syaker <60866313+syaker@users.noreply.github.com>
2020-09-07 01:56:50 -0500

    Anadiendo imagen a cada chat personalizado

--- src/containers/MainChat.jsx
+++ src/containers/MainChat.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import HeaderComponent from "../components/HeaderComponent";
+import { getAllChats, getInfoUser } from '../API/chat';
 import MessageContainer from "../components/MessageContainer";
-import lucy from "../assets/icons/lucy.png";
+import { auth } from '../firebase';
+import { useParams } from "react-router-dom";
 
 function MainChat() {
-  return (
-    <div>
-      <HeaderComponent titleHeader="Chompas Lucy" imageIcon={lucy} />
 
-      <MessageContainer />
-    </div>
-  );
+	const userActive = auth.currentUser.uid;
+	let { id: idReceiver } = useParams();
+	const [infoUsers, setInfoUsers] = useState([]);
+	const getChats = async () => await getAllChats();
+	useEffect(() => {
+		getChats().then((chats) => {
+			const idsPerVerificate = chats.map((id) => { return (id.IDChat.split("_")) });
+			const idsToShow = [];
+			idsPerVerificate.forEach((obj) => {
+				obj.forEach((id) => {
+					if (id !== userActive && id === idReceiver) idsToShow.push(id);
+				})
+			})
+			const promises = idsToShow.map((id) => {
+				return getInfoUser(id).then((doc) => {
+					return {
+						photoUserUrl: doc.photoUserUrl,
+						nameCompany: doc.nameCompany,
+					}
+				})
+			})
+			return Promise.all(promises)
+		}).then((arrUser) => {
+			setInfoUsers(arrUser[0])})
+	}, []);
+	return (
+		<div>
+			<HeaderComponent titleHeader={infoUsers.nameCompany} imageIcon={infoUsers.photoUserUrl} />
+			<MessageContainer />
+		</div>
+	);
 }
 
 export default MainChat;
